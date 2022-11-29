f4a7b30c80330152754686d216f5d393f7a952b2
Jaebum Lee <jaebumlee@Jaebums-MacBook-Pro.local>
2022-11-29 17:09:45 -0500

    delete recipe function

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,6 +5,7 @@ module.exports = {
     new: newRecipe,
     create,
     show,
+    delete: deleteRecipe
 
 }
 
@@ -37,5 +38,12 @@ function create(req, res) {
 function show(req, res) {
     Recipe.findById(req.params.id, function(err, recipe) {
         res.render('recipes/show', { title: recipe.title, recipe })
+    })
+}
+
+function deleteRecipe(req, res) {
+    Recipe.findById(req.params.rId, function(err, recipe) {
+      recipe.remove(req.params.rId)
+      res.redirect('/recipes')
     })
 }
--- routes/recipes.js
+++ routes/recipes.js
@@ -6,5 +6,6 @@ router.get('/', recipesCtrl.index);
 router.get('/new', recipesCtrl.new);
 router.get('/:id', recipesCtrl.show);
 router.post('/', recipesCtrl.create);
+router.delete('/:rId/', recipesCtrl.delete)
 
 module.exports = router;
